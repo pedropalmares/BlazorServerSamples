bda723c061ea3bc13573537d33adfab9fa80d71e
Pedro Palmares <pedro@valuenegotiation.tech>
2020-07-24 16:59:53 +0100

    Added Communication between components using a state container and forcing the parent component to refresh

---
+++ Data/VehicleState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSamples.Data
+{
+    public class VehicleState
+    {
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public string SelectedVehicle { get; set; }
+
+        public void SetVehicle (string vehicle)
+        {
+            SelectedVehicle = vehicle;
+            NotifyStateChanged();
+        }
+    }
+}
